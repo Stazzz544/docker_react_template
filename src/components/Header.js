2d7e226dbcc71c568fdf55443197bdf68789cf2e
Basilevs <stazzzdzr@gmail.com>
2023-02-20 08:07:28 +0300

    docker_template

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const Header = () => {
+    return(
+        <div className="header">
+          <h1>{process.env.REACT_APP_NAME}</h1>
+            <h1>123123</h1>
+        </div>
+    )
+}
